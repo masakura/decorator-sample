0504ba440545eab21d92062d4760f334cf9ba18d
Tomo Masakura <tomo.masakura@gmail.com>
2017-04-12 14:12:59 +0900

    RawGitHubApi に実装をコピー

--- src/main/java/labo/github/RawGitHubApi.java
+++ src/main/java/labo/github/RawGitHubApi.java
@@ -1,4 +1,23 @@
 package labo.github;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
 public class RawGitHubApi extends GitHubApi {
+    private final WebTarget target;
+
+    public RawGitHubApi() {
+        Client client = ClientBuilder.newClient();
+        target = client.target("https://api.github.com/search/repositories");
+    }
+
+    @Override
+    public RepositoriesResult searchRepositories(String keyword) {
+        return target
+                .queryParam("q", keyword)
+                .request(MediaType.APPLICATION_JSON_TYPE)
+                .get(RepositoriesResult.class);
+    }
 }
