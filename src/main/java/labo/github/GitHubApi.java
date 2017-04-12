181d6821f9ec13af29740a5c7095565937377c8c
Tomo Masakura <tomo.masakura@gmail.com>
2017-04-12 14:13:59 +0900

    GitHubApi クラスを abstract 化

--- src/main/java/labo/github/GitHubApi.java
+++ src/main/java/labo/github/GitHubApi.java
@@ -6,18 +6,6 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
 @SuppressWarnings("WeakerAccess")
-public class GitHubApi {
-    private final WebTarget target;
-
-    public GitHubApi() {
-        Client client = ClientBuilder.newClient();
-        target = client.target("https://api.github.com/search/repositories");
-    }
-
-    public RepositoriesResult searchRepositories(String keyword) {
-        return target
-                .queryParam("q", keyword)
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .get(RepositoriesResult.class);
-    }
+public abstract class GitHubApi {
+    public abstract RepositoriesResult searchRepositories(String keyword);
 }
